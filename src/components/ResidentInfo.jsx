3e4bebb5fb38b78e4dbe929a2a7420bc31250b6f
Daniel1264 <ramosperca015@gmail.com>
2022-10-02 20:57:57 -0500

    styles status of characters in residentinfo

--- src/components/ResidentInfo.jsx
+++ src/components/ResidentInfo.jsx
@@ -11,10 +11,11 @@ const ResidentInfo = ({url}) => {
     <div className='location_information_resident'>
         <div >
             <img src={res?.image} />
+            <h5 className='location_information_reident_status'><p> {res?.status}</p></h5>
+            <div className={res?.status === 'Dead' ? 'dead' : 'alive'}></div>
         </div>
         <ul>
             <h2>{res?.name}</h2>
-            <li>Status<span><p className={res?.status === 'Dead' ? 'dead' : 'alive'}> {res?.status}</p></span></li>
             <li>Origen<span><p>{ res && res.origin.name}</p></span></li>
             <li>Episodios<span><p>{res && res.episode.length}</p></span></li>
         </ul>
